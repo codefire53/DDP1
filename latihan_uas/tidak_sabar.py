a2e19a42142998b086398f5f5626e71368a747b5
Mahardika Krisna Ihsani <krisnaihsani@gmail.com>
2018-12-20 21:03:14 +0700

    Update tidak_sabar.py

--- latihan_uas/tidak_sabar.py
+++ latihan_uas/tidak_sabar.py
@@ -1,9 +1,11 @@
 from tkinter import *
 def main():
 	main=Tk()
+	#Menginisiasi string label dan button di tkinter
 	labelText = StringVar()
 	labelText.set('Mohon bersabar, ini tryout')
 	label=Label(main,textvariable=labelText)
+	#Jika button diklik, makan nilai label akan berubah
 	button=Button(main,text='Tidak sabar',command=lambda : labelText.set("Anda tidak sabar"))
 	label.pack()
 	button.pack()
