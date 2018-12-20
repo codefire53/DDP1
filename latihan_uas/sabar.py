5e47cfd02bf498f4676d36ab2a36a1a8b9f98b50
Mahardika Krisna Ihsani <krisnaihsani@gmail.com>
2018-12-20 21:02:38 +0700

    Update sabar.py

--- latihan_uas/sabar.py
+++ latihan_uas/sabar.py
@@ -1,10 +1,11 @@
 from tkinter import *
 def main():
 	main=Tk()
-	
+	#Menginisiasi string label dan button di tkinter
 	labelText = StringVar()
 	labelText.set('Mohon bersabar, ini tryout')
 	label=Label(main,textvariable=labelText)
+	#Jika button diklik, makan nilai label akan berubah
 	button=Button(main,text='Sabar',command=lambda : labelText.set("Terima kasih, Anda sabar"))
 	label.pack()
 	button.pack()
